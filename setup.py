b4490debf3e43fab625fbc2df75dfcd46563faf7
HwaiTengTeoh <teoh0821@gmail.com>
2022-03-23 16:51:59 +0800

    python package for text preprocessing

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open('README.md','r') as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name = 'preprocess_text', #should be unique globally to avoid package name conflict
+    version = '0.0.1',
+    author = 'Hwai Teng Teoh',
+    author_email = 'teoh0821@gmail.com',
+    description = 'This is preprocessing package',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    packages = setuptools.find_packages(),
+    classifiers = [
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'],
+    python_requires = '>=3.5'
+)
